96ea0fe42792389e6491c1dff6ed425cb8efd95c
ali ismail <aliismail106@gmail.com>
2023-05-29 23:58:46 +0300

    Mohamed Mamdouh's services implemented.

---
+++ services/OfferService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using WindowsFormsApp1;
+
+namespace Hotel_Management_System.services
+{
+    public class OfferService
+    {
+        private static readonly string ConnectionString = SqlService.ConnectionString;
+
+        public static List<Offer> GetOffers()
+        {
+            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM offer";
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    List<Offer> dataRows = new List<Offer>();
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32("id");
+                        int discountPercentage = reader.GetInt32("discountPercentage");
+                        DateTime expirationDate = reader.GetDateTime("expirationDate");
+                        Offer offerData = new Offer(id, discountPercentage, expirationDate);
+                        dataRows.Add(offerData);
+                    }
+                    connection.Close();
+                    return dataRows;
+                }
+            }
+        }
+
+        public static bool CreateOffer(Offer offerData)
+        {
+            try
+             {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "INSERT INTO offer (discountPercentage, expirationDate) VALUES (@discountPercentage, @expirationDate)";
+                    command.Parameters.AddWithValue("@discountPercentage", offerData.discountPercentage);
+                    command.Parameters.AddWithValue("@expirationDate", offerData.expirationDate);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine("Error creating offer: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool UpdateOffer(Offer offerData)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "UPDATE offer SET discountPercentage = @discountPercentage, expirationDate = @expirationDate WHERE id = @id";
+                    command.Parameters.AddWithValue("@discountPercentage", offerData.discountPercentage);
+                    command.Parameters.AddWithValue("@expirationDate", offerData.expirationDate);
+                    command.Parameters.AddWithValue("@id", offerData.id);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine("Error updating offer: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool DeleteOffer(int offerId)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "DELETE FROM offer WHERE id = @offerId";
+                    command.Parameters.AddWithValue("@offerId", offerId);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine("Error deleting offer: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ services/PaymentService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using WindowsFormsApp1;
+
+namespace Hotel_Management_System.services
+{
+    public class PaymentService
+    {
+        private static readonly string ConnectionString = SqlService.ConnectionString;
+
+        public static List<Payment> GetPayments()
+        {
+            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+             {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM payment";
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    List<Payment> dataRows = new List<Payment>();
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32("id");
+                        string name = reader.GetString("name");
+                        string description = reader.GetString("description");
+                        Payment payment = new Payment(id, name, description);
+                        dataRows.Add(payment);
+                    }
+                    connection.Close();
+                    return dataRows;
+                }
+            }
+        }
+
+        public static bool CreatePayment(Payment payment)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "INSERT INTO payment (name, description) VALUES (@name, @description)";
+                    command.Parameters.AddWithValue("@name", payment.name);
+                    command.Parameters.AddWithValue("@description", payment.description);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine("Error creating payment: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool UpdatePayment(Payment payment)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "UPDATE payment SET name = @name, description = @description WHERE id = @id";
+                    command.Parameters.AddWithValue("@name", payment.name);
+                    command.Parameters.AddWithValue("@description", payment.description);
+                    command.Parameters.AddWithValue("@id", payment.id);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine("Error updating payment: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool DeletePayment(int paymentId)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "DELETE FROM payment WHERE id = @paymentId";
+                    command.Parameters.AddWithValue("@paymentId", paymentId);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine("Error deleting payment: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
